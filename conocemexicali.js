5c8578b7327d015132af76625fcb4503afb0c19d
D0MORENO <andelir0190@gmail.com>
2025-03-11 19:26:45 -0600

    Add files via upload

---
+++ conocemexicali.js
@@ -0,0 +1,14 @@
+// Función para mostrar un mensaje de bienvenida
+window.onload = function() {
+    alert("¡Bienvenido a Conoce Mexicali! 🌞");
+}
+
+// Función para cambiar el fondo de la página
+function cambiarFondo() {
+    document.body.style.background = "linear-gradient(to right, blue, green)";
+}
+
+// Función para mostrar un mensaje de más información sobre cada lugar
+function mostrarInfo(lugar) {
+    alert("¡Gracias por visitar " + lugar + "!");
+}
